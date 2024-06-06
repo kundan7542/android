a825601bd151bdaec63cf0ba486a2a744be466bb
kundan <kundansingh754285@gmail.com>
2024-06-06 17:33:19 +0530

    android project

---
+++ lib/hello_world.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class HelloWorld extends StatefulWidget {
+  const HelloWorld({super.key});
+
+  @override
+  State<HelloWorld> createState() => _HelloWorldState();
+}
+
+class _HelloWorldState extends State<HelloWorld> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello World');
+  }
+}
